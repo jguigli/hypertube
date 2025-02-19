03e94e1835cff555db4d023d8afe57ed43276ff6
Charles <contact@charles-mariot.fr>
2025-02-19 17:59:04 +0100

    Front : navbar, sidebar en cours

--- frontend/app/src/components/Layout.tsx
+++ frontend/app/src/components/Layout.tsx
@@ -1,15 +1,37 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import { useState } from "react";
+import Sidebar from "./Sidebar";
 
 export default function Layout() {
+
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+        const layout = document.getElementById("layout");
+        if (layout) {
+            // Change the layout grid columns
+            layout.classList.toggle("grid-cols-[250px_1fr]");
+            layout.classList.toggle("grid-cols-[50px_1fr]");
+        }
+    };
+
     return (
-        <>
-            <Navbar />
-            <main>
+        <div
+            id="layout"
+            className="grid
+                grid-rows-[auto_1fr_auto]
+                grid-cols-[50px_1fr]
+                min-h-screen"
+        >
+            <Navbar toggle_menu={toggleMenu} />
+            <Sidebar open={menuOpen} toggle_menu={toggleMenu} />
+            <main className="col-span-1 row-span-2 row-start-2 col-start-2">
                 <Outlet />
             </main>
             <Footer />
-        </>
+        </div>
     );
 }
