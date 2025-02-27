397b21ef04dd8c11ed56f5642c6a33522cd567df
Silveratis0922 <tristan0922@hotmail.fr>
2025-02-27 02:17:49 +0100

    VideoView ok mais besoin de petites modif

--- frontend/app/src/main.tsx
+++ frontend/app/src/main.tsx
@@ -10,13 +10,16 @@ import '@fontsource/roboto/700.css';
 import { ThemeProvider } from '@mui/material'
 import theme from './styles/MUIThemeProvider.tsx'
 import { SearchProvider } from './contexts/SearchContext.tsx'
+import { VideoProvider } from './contexts/VideoContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <ThemeProvider theme={theme}>
             <AuthProvider>
                 <SearchProvider>
-                    <AppRouter />
+                    <VideoProvider>
+                        <AppRouter />
+                    </VideoProvider>
                 </SearchProvider>
             </AuthProvider>
         </ThemeProvider>
