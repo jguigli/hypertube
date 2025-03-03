102c4054296f228f3cc2bce0392fb629519f0790
cmariot <contact@charles-mariot.fr>
2025-03-03 10:57:22 +0100

    Config nginx

--- frontend/app/src/services/LoginService.tsx
+++ frontend/app/src/services/LoginService.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // Axios configuration
-axios.defaults.baseURL = "http://localhost:8080/api";
+axios.defaults.baseURL = "http://localhost:3000/api";
 
 export default class LoginService {
 
