f335e44ce8bfe0bf90b54e0e900e83d8c9aff0fc
GUIGLI <joel.guigli@gmail.com>
2025-04-28 15:07:32 +0200

    add GET /movies/id/comments, mandatory

--- backend/api/movies/resources.py
+++ backend/api/movies/resources.py
@@ -51,6 +51,7 @@ from ..database import SessionLocal
 from ..websocket.websocket_manager import manager_websocket
 import requests
 from api.config import JACKETT_API_KEY
+from api.comments.schemas import Comment
 
 
 router = APIRouter(tags=["Movies"])
@@ -581,3 +582,18 @@ async def get_subtitles(
         media_type="application/zip",
         headers={"Content-Disposition": "attachment; filename=files.zip"}
     )
+
+
+@router.get('/movies/{movie_id}/comments', response_model=List[Comment])
+async def get_movie_comments(
+    movie_id: int,
+    current_user: Annotated[User, Depends(security.get_current_user)],
+    db: Session = Depends(get_db)
+):
+    movie = get_movie_by_id(db, movie_id)
+    if not movie:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Invalid movie"
+        )
+    return movie.comments
